2cbb74062ae1537445b7141e35c9a8761d3b57e8
MaxiCB <aaroncb@aaroncb.com>
2020-01-21 11:00:14 -0600

    Implement DummyData - PostPage - SearchBar
    Imported all required data into App.js to complete required steps inside

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
+import PostsPage from "./components/PostsContainer/PostsPage";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
+import { dummyData } from "./dummy-data";
 // import the PostsPage and SearchBar and add them to the App
 // import dummydata
 
@@ -13,6 +16,8 @@ const App = () => {
   // set up state for the dummy data and pass to your PostsPage
   return (
     <div className="App">
+      <SearchBar/>
+      <PostsPage/>
       {/* Add components here  and pass props where appropriate */}
     </div>
   );
