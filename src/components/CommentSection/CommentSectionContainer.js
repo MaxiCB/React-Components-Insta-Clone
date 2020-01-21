eb1f9d6dfed3cc036b14bf4ed615065743553824
MaxiCB <aaroncb@aaroncb.com>
2020-01-21 11:22:11 -0600

    ADD Comment Data Passing
    - Implemented Comments Data
    - Implemented Likes Data
    - Added data handling into comment to display comments for Post
    - Added passing post data into likes to display likes

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -1,15 +1,16 @@
-// You will add code to this file
 import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
 const CommentSection = props => {
-  // Add state for the comments
+  const [comments, setComments] = useState(props.comments)
 
   return (
     <div>
-      {/* map through the comments data and return the Comment component */}
+      {comments.map((comment) => {
+        return <Comment comment={comment}/>
+      })}
       <CommentInput />
     </div>
   );
